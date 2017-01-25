d6ddd39aaa530ed136440c862124e8551e5c497e
Stephan Druskat <mail@sdruskat.net>
2017-01-25 15:19:55 +0100

    Change constructor

--- src/main/java/org/corpus_tools/peppermodules/flex/readers/FLExDocumentReader.java
+++ src/main/java/org/corpus_tools/peppermodules/flex/readers/FLExDocumentReader.java
@@ -1,8 +1,8 @@
 package org.corpus_tools.peppermodules.flex.readers;
 
-import org.corpus_tools.peppermodules.flex.FLExImporter;
 import org.corpus_tools.peppermodules.flex.exceptions.DocumentSAXParseFinishedEvent;
 import org.corpus_tools.peppermodules.flex.model.FLExText;
+import org.corpus_tools.salt.common.SDocument;
 import org.corpus_tools.salt.common.SDocumentGraph;
 import org.corpus_tools.salt.core.SMetaAnnotation;
 import org.corpus_tools.salt.util.SaltUtil;
@@ -25,46 +25,60 @@ public class FLExDocumentReader extends DefaultHandler2 implements FLExText {
 	private final SDocumentGraph graph;
 	private boolean isDocument = false;
 
+	private SDocument doc;
+
 	/**
 	 * @param graph
+	 * @param document 
 	 */
-	public FLExDocumentReader(SDocumentGraph graph) {
-		this.graph = graph;
+	public FLExDocumentReader(SDocument document) {
+		this.doc = document; 
+		this.graph = document.getDocumentGraph();
 	}
 
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-		if (TAG_DOCUMENT.equals(qName)) {
-		}
-		else if (TAG_INTERLINEAR_TEXT.equals(qName)) {
-			SMetaAnnotation guidAnnotation = graph.getDocument().getMetaAnnotation(FLExImporter.FLEX_NAMESPACE + SaltUtil.NAMESPACE_SEPERATOR + FLExImporter.FLEX_DOCUMENT_GUID);
-			if (guidAnnotation != null && attributes.getValue(FLExImporter.FLEX_DOCUMENT_GUID).equals(guidAnnotation.getValue_STEXT())) {
+		if (TAG_INTERLINEAR_TEXT.equals(qName)) {
+			SMetaAnnotation guidAnnotation = graph.getDocument().getMetaAnnotation(FLExText.FLEX_NAMESPACE + SaltUtil.NAMESPACE_SEPERATOR + FLExText.FLEX__GUID_ATTR);
+			if (guidAnnotation != null && attributes.getValue(FLExText.FLEX__GUID_ATTR).equals(guidAnnotation.getValue_STEXT())) {
 				isDocument = true;
+				System.err.println("In doc " + doc.getName());
 			}
 		}
 		if (isDocument) {
-			if (TAG_PARAGRAPHS.equals(qName)) {
-			}
-			else if (TAG_PARAGRAPH.equals(qName)) {
-			}
-			else if (TAG_PHRASES.equals(qName)) {
-			}
-			else if (TAG_PHRASE.equals(qName)) {
-			}
-			else if (TAG_WORDS.equals(qName)) {
-			}
-			else if (TAG_WORD.equals(qName)) {
-			}
-			else if (TAG_MORPHEMES.equals(qName)) {
-			}
-			else if (TAG_MORPH.equals(qName)) {
-			}
-			else if (TAG_LANGUAGES.equals(qName)) {
-			}
-			else if (TAG_LANGUAGE.equals(qName)) {
-			}
-			else if (TAG_ITEM.equals(qName)) {
-			}
+//			if (TAG_PARAGRAPHS.equals(qName)) {
+//				System.err.println("In doc " + doc.getName());
+//			}
+//			else if (TAG_PARAGRAPH.equals(qName)) {
+//				System.err.println("In doc " + doc.getName());
+//			}
+//			else if (TAG_PHRASES.equals(qName)) {
+//				System.err.println("In doc " + doc.getName());
+//			}
+//			else if (TAG_PHRASE.equals(qName)) {
+//				System.err.println("In doc " + doc.getName());
+//			}
+//			else if (TAG_WORDS.equals(qName)) {
+//				System.err.println("In doc " + doc.getName());
+//			}
+//			else if (TAG_WORD.equals(qName)) {
+//				System.err.println("In doc " + doc.getName());
+//			}
+//			else if (TAG_MORPHEMES.equals(qName)) {
+//				System.err.println("In doc " + doc.getName());
+//			}
+//			else if (TAG_MORPH.equals(qName)) {
+//				System.err.println("In doc " + doc.getName());
+//			}
+//			else if (TAG_LANGUAGES.equals(qName)) {
+//				System.err.println("In doc " + doc.getName());
+//			}
+//			else if (TAG_LANGUAGE.equals(qName)) {
+//				System.err.println("In doc " + doc.getName());
+//			}
+//			else if (TAG_ITEM.equals(qName)) {
+//				System.err.println("In doc " + doc.getName());
+//			}
 		}
 	}
 	
