723322ea52f12c44272fa0097446edd7159b3e81
Stephan Druskat <mail@sdruskat.net>
2017-11-10 11:26:23 +0100

    Change XSD validation log levels

--- src/main/java/org/corpus_tools/peppermodules/flex/FLExImporter.java
+++ src/main/java/org/corpus_tools/peppermodules/flex/FLExImporter.java
@@ -82,11 +82,11 @@ public class FLExImporter extends PepperImporterImpl implements PepperImporter{
 				schema = sf.newSchema(xsd);
 				Validator validator = schema.newValidator();
 				validator.validate(new StreamSource(corpusFile));
-				logger.info(corpusFile.getAbsolutePath() + " has validated successfully against '" + xsd.getFile() + "'.");
+				logger.debug(corpusFile.getAbsolutePath() + " has validated successfully against '" + xsd.getFile() + "'.");
 				// Every .flextext file is a *corpus*, not a *document*!
 			}
 			catch (SAXException | IOException e) {
-				logger.error(corpusFile.getAbsolutePath() + " has not validated successfully against '" + xsd.getFile() + "'! Ignoring file.", e);
+				logger.warning(corpusFile.getAbsolutePath() + " has not validated successfully against '" + xsd.getFile() + "'! Ignoring file.", e);
 				return;
 			} 
 			// Parse FLEXText file once to create documents
